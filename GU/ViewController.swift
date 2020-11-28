5e5b2306755b26a757ca9d98133b90221a00e5b5
nsbien <nicole.bien22@gmail.com>
2020-11-27 22:56:06 -0800

    added icons

--- GU/ViewController.swift
+++ GU/ViewController.swift
@@ -6,6 +6,9 @@
 //  Sources:
 //      PENCIL ICON
 //      <div>Icons made by <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+//  OUTLOOK ICON
+//  SCHEDULE ADD
+//  <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 //
 
 import UIKit
@@ -15,7 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        navigationController?.navigationBar.barTintColor = UIColor(red: 0.30, green: 0.47, blue: 0.64, alpha: 1.00)
+        navigationController?.navigationBar.barTintColor = UIColor(red: 0.09, green: 0.30, blue: 0.45, alpha: 1.00)
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)]
     }
 }
