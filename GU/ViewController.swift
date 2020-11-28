73e6c32d56ac43779639665b6d158cc521dcef88
nsbien <nicole.bien22@gmail.com>
2020-11-27 17:44:34 -0800

    added buttons to home screen

--- GU/ViewController.swift
+++ GU/ViewController.swift
@@ -3,6 +3,9 @@
 //  GU
 //
 //  Created by Nicole Bien on 11/27/20.
+//  Sources:
+//      PENCIL ICON
+//      <div>Icons made by <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 //
 
 import UIKit
@@ -12,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        navigationController?.navigationBar.barTintColor = UIColor(red: 0.09, green: 0.30, blue: 0.45, alpha: 1.00)
+        navigationController?.navigationBar.barTintColor = UIColor(red: 0.30, green: 0.47, blue: 0.64, alpha: 1.00)
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)]
     }
 
