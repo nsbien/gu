7bc787d6262dd944ce77f5b3caecf34627c61dae
nsbien <nicole.bien22@gmail.com>
2020-11-27 16:52:10 -0800

    added background color

--- GU/ViewController.swift
+++ GU/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.navigationBar.barTintColor = UIColor(red: 0.09, green: 0.30, blue: 0.45, alpha: 1.00)
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)]
     }
 
 
