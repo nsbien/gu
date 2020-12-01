0aef5747c1bf081668b1af28f1b42e7047c338bf
nsbien <nicole.bien22@gmail.com>
2020-12-01 14:43:19 -0800

    added add class page

--- GU/AddClassViewController.swift
+++ GU/AddClassViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class AddClassViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var courseName: UILabel!
+    @IBAction func inputCourseName(_ sender: Any) {
+    }
+    @IBOutlet var startTime: UILabel!
+    @IBAction func inputStartPicker(_ sender: Any) {
+    }
+    @IBOutlet weak var endTIme: UILabel!
+    @IBAction func inputEndPicker(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
     }
-    */
+    
 
 }
--- GU/Class.swift
+++ GU/Class.swift
@@ -9,13 +9,11 @@ import Foundation
 //https://stackoverflow.com/questions/43405959/launch-a-local-notification-at-a-specific-time-in-ios
 
 struct Class {
-    var subject: String
     var name: String
     var meetingTime = DateInterval()
     var location: String
     
-    init(withSubject subject: String, withName name: String, withMeetingTime meetingTime: DateInterval, withLocaiton location: String) {
-        self.subject = subject
+    init(withName name: String, withMeetingTime meetingTime: DateInterval, withLocaiton location: String) {
         self.name = name
         self.meetingTime = meetingTime
         self.location = location
