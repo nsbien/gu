4d50e61dedf3e6ba1fd956dc187cb7276c2cc96a
nsbien <nicole.bien22@gmail.com>
2020-12-16 21:55:13 -0800

    created last page

---
+++ GU/EmergencyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmergencyViewController.swift
+//  GU
+//
+//  Created by Nicole Bien on 12/16/20.
+//
+
+import UIKit
+
+class EmergencyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
