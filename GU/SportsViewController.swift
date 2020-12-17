233745d5c95f583892d16676d3c0bba17f7ffad2
Kyle Pavey <kylepavey1@gmail.com>
2020-12-16 22:53:48 -0800

    changes sports link

--- GU/SportsViewController.swift
+++ GU/SportsViewController.swift
@@ -20,7 +20,7 @@ class SportsViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let url = URL(string: "https://www.google.com/search?q=gonzaga+basketball&oq=gonzaga+ba&aqs=chrome.1.69i57j0i131i355i433i457j0j0i131i433j0j0i395i433j69i60j69i61.2992j1j4&sourceid=chrome&ie=UTF-8#sie=t;/m/03c_p38;3;/g/11ckvf54fz;mt;fp;1;;")!
+        let url = URL(string: "https://gozags.com/calendar?date=12/16/2020&vtype=list")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = false
     }
